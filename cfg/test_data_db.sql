c897a6b76d11098ffde218dbac15963149bfce8e
kevin-strobel <kevinstrobel1@gmx.de>
2014-03-07 21:53:32 +0100

    SQL: Case sensitivity.

--- cfg/test_data_db.sql
+++ cfg/test_data_db.sql
@@ -49,7 +49,7 @@ INSERT INTO Kategorienfilter VALUES
 ('Kevin09', 'Projektmanagement', true),
 ('Kevin09', 'Wissenschaftliches Arbeiten', true);
 
-INSERT INTO freundesliste VALUES
+INSERT INTO Freundesliste VALUES
 ('Kevin01', 'admin'),
 ('Kevin01', 'Kevin02'),
 ('Kevin01', 'Kevin04'),
@@ -57,13 +57,13 @@ INSERT INTO freundesliste VALUES
 ('Kevin01', 'Kevin09');
 
 
-INSERT INTO unterkategorie VALUES
+INSERT INTO Unterkategorie VALUES
 ('UK 1 zu IT-Management','IT-Management'),
 ('UK 2 zu IT-Management','IT-Management'),
 ('UK 1 zu Programmierung','Programmierung'),
 ('UK 2 zu Programmierung','Programmierung');
 
-INSERT INTO frage(kategorieName,unterkategorieName,flagFragenTypMult,frage,antwortmoeglichkeit1,antwortmoeglichkeit2,antwortmoeglichkeit3,antwortmoeglichkeit4,wahrheitAntwortmoeglichkeit1,wahrheitAntwortmoeglichkeit2,wahrheitAntwortmoeglichkeit3,wahrheitAntwortmoeglichkeit4,flagFrageValidiert) VALUES
+INSERT INTO Frage(kategorieName,unterkategorieName,flagFragenTypMult,frage,antwortmoeglichkeit1,antwortmoeglichkeit2,antwortmoeglichkeit3,antwortmoeglichkeit4,wahrheitAntwortmoeglichkeit1,wahrheitAntwortmoeglichkeit2,wahrheitAntwortmoeglichkeit3,wahrheitAntwortmoeglichkeit4,flagFrageValidiert) VALUES
 ('IT-Management','UK 1 zu IT-Management',false,'Welche Gase wurden behandelt?','Exert','Inert','Exposé','Imposé',false,true,false,false,true),
 ('IT-Management','UK 2 zu IT-Management',true,'Was heißt/hieß RAID?','Redundant Array of Independent Devices','Redundant Array of Inexpensive Disks','Redundant Array of Independent Disks','Redundant Array of Inhuman Disks',false,true,true,false,true),
 ('IT-Management','UK 1 zu IT-Management',false,'Wie ist Datenschutz anzuwenden?','Erst dann, wenn es zu spät ist.','Nach dem ersten Datenschutzbruch','Präventiv','Gar nicht',false,false,true,false,true),
@@ -71,10 +71,10 @@ INSERT INTO frage(kategorieName,unterkategorieName,flagFragenTypMult,frage,antwo
 ('Programmierung', 'UK 2 zu Programmierung',false,'Welche Schleifen-Art gibt es in C++ nicht?', 'For', 'Do-Until','While','Do-While',false,true,false,false,true),
 ('Programmierung', 'UK 1 zu Programmierung',false,'Welcher Assembly-Befehl kopiert register ax nach bx (Intel-Syntax)?', 'mov bx,ax','cpy bx,ax', 'dup bx,ax','copy bx,ax',true,false,false,false,true);
 
-INSERT INTO spiel VALUES
+INSERT INTO Spiel VALUES
 (1,'M','Kevin02','Kevin01',null,null,'Kevin01',1,'P','2014-02-18 17:10:47');
 
-INSERT INTO runde VALUES
+INSERT INTO Runde VALUES
 (1,1,1),
 (2,1,2),
 (3,1,3),
@@ -82,7 +82,7 @@ INSERT INTO runde VALUES
 (5,1,5),
 (6,1,6);
 
-INSERT INTO antwort VALUES
+INSERT INTO Antwort VALUES
 (1,1,'Kevin02',true,false,false,false,true,false),
 (2,1,'Kevin02',false,true,false,false,true,false),
 (3,1,'Kevin02',true,false,false,false,true,false),
