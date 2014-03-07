0b0c82b82375f3fe191b3d240685e00e2a167931
Paul Kotte <p.kotte93@gmx.de>
2014-03-07 22:00:21 +0100

    App: Add AddFriend Button implement sort algorithmn ...

--- Studiduell_Prototyp/www/js/neuesSpiel.js
+++ Studiduell_Prototyp/www/js/neuesSpiel.js
@@ -1,3 +1,5 @@
+init();
+
 function init() {
 	loadFriendslistFromServer();
 }
@@ -11,29 +13,28 @@ function loadFriendslistFromServer() {
 /* 	1. hole die Freundesliste des Users vom Server
 		2. füge dem html Dokument für jeden user in der Freundesliste einen Eintrag "Freund" hinzu.
 	*/
-		$.ajax( {
+/* 		$.ajax( {
 		url:serverURL + "Studiduell/settings/friends",
 		type:"GET",
 		success:function(obj){addFriendToScreen(obj);},
 		error:function(obj){alert(JSON.stringify(obj));}
-	});
+	}); */
 	
-	/* //Test (Testdaten ohne Serveranbindung!)
+	//Test (Testdaten ohne Serveranbindung!)
 	var tmpServerData = new Array("Anita", "Bettina", "Christa", "Doris");
-	addFriendToList(tmpServerData); */
+	addFriendToList(tmpServerData); 
 }
 
 function addFriendToList(obj){
-	//füge HTML ein:
-	//TODO in den Container hinzufügen
+	//Freundesliste befüllen
 	for(var i=0;i<obj.length;i++){
-	$("#freundeslistErweitern").append("<li class='topcoat-list__item custom_List_item' ontouchend='createNewGameFriendlist(friendName)'>"+obj[i]+"</li>");
+	$("#freundeslisteErweitern").append("<li class='topcoat-list__item custom_List_item' onclick='createNewGameFriendlist(this)'>"+obj[i]+"</li>");
 	}
 }
 
 
 function createNewGameFriendlist(friendName){
-$.ajax( {
+/*$.ajax( {
 		url:serverURL + "game/create/with/" + friendName,
 		type:"POST",
 		success:function(){openHomeView();},
@@ -41,7 +42,7 @@ $.ajax( {
 		});
 		
 		//Test
-		//openHomeView();
+		*/openHomeView();
 		
 }
 
@@ -54,7 +55,7 @@ Wenn kein passendes Spiel vorhanden ist, starte ein neues Spiel gegen "Unbekannt
 --> beim laden des homescreens wird das Spiel angezeigt!*/
 
 	// Schicke Anfrage an Server
-	$.ajax( {
+/*	$.ajax( {
 		url:serverURL + "Studiduell/game/create/random",
 		type:"POST",
 		success:function(){openHomeView();},
@@ -62,7 +63,7 @@ Wenn kein passendes Spiel vorhanden ist, starte ein neues Spiel gegen "Unbekannt
 		});
 		
 		//Test
-		//openHomeView();
+		*/openHomeView();
 }
 
 
@@ -71,5 +72,4 @@ var homeScreenView = new steroids.views.WebView("html/home.html");
 	steroids.layers.push(homeScreenView);
 }
 
-
-document.addEventListener("deviceready", init, false);
+//document.addEventListener("deviceready", init, false);
