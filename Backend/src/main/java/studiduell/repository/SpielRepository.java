ce87414f4da82771719ccd1b233d4f4f0c3a3552
kevin.strobel <kevinstrobel1@gmx.de>
2014-03-18 19:40:15 +0100

    Server: sync also sends 5 last closed/abandoned games + FIXED bug in sync hqls.

--- Backend/src/main/java/studiduell/repository/SpielRepository.java
+++ Backend/src/main/java/studiduell/repository/SpielRepository.java
@@ -2,6 +2,8 @@ package studiduell.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +17,9 @@ public interface SpielRepository extends JpaRepository<SpielEntity, Integer> {
 	Integer getWithUserAndOpponentInStatus(@Param("user") UserEntity user,
 			@Param("opponent") UserEntity opponent, @Param("status") List<SpielstatusEntity> spielstatus);
 	
-	@Query("FROM SpielEntity s WHERE s.spieler1 = :player OR s.spieler2 = :player AND s.spielstatusName IN (:status)")
+	@Query("FROM SpielEntity s WHERE (s.spieler1 = :player OR s.spieler2 = :player) AND s.spielstatusName IN (:status)")
 	List<SpielEntity> getWithUserInStatus(@Param("player") UserEntity spieler, @Param("status") List<SpielstatusEntity> spielstatus);
+	
+	@Query("FROM SpielEntity s WHERE (s.spieler1 = :player OR s.spieler2 = :player) AND s.spielstatusName IN (:status) ORDER BY s.spielID DESC")
+	Page<SpielEntity> getWithUserInStatusOrderBySpielIDDesc(@Param("player") UserEntity spieler, @Param("status") List<SpielstatusEntity> spielstatus, Pageable pageable);
 }
